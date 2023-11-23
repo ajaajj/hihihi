1439c9c44c58e37ef076e88b86297cd1ba09b5b0
TinyRiki <2197990324@qq.com>
2023-11-23 14:59:36 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tom">我是App</div>
+</template>
+
+<script lang="ts" setup>
+const a = "1111";
+</script>
+
+<style scoped>
+.tom {
+  width: 100px;
+  height: 200px;
+}
+</style>
